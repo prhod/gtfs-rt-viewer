bbdfa4a37a2fd8cddcbab4dc28cb8cc912dd17a0
Pascal Rhod <pascal.rhod@kisio.com>
2020-08-20 16:31:06 +0200

    [front] rename component

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -70,7 +70,7 @@
     import { mapState, mapActions } from 'vuex'
 
     export default {
-        name: 'HelloWorld',
+        name: 'Home',
         components: { JsonViewer, BBoxGlMap },
 
         data: () => ({
